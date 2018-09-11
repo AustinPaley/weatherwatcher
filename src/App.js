99bf44f49e6872774cca157d5e8b9d322f649c41
AustinPaley <austinpaley32@gmail.com>
2018-09-11 17:53:25 -0400

    Basic Sass Layout + Snow Background Video

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import snow from './img/snow.mp4'
+import './css/style.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <div className="bg-video">
+            <video className="bg-video__content" autoPlay muted loop>
+              <source src={snow} type="video/mp4" />
+              Your browser is not supported!
+            </video>
+          </div>
       </div>
     );
   }
