3393d42514ecb8eeddaed02486bb0db0d0b1d50a
AustinPaley <austinpaley32@gmail.com>
2018-09-12 17:20:30 -0400

    Zip Input CSS Updates

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import snow from './img/snow.mp4'
 import './css/style.css';
 import * as Zipcodes from './zipcodes/zip.json';
 import FetchAdapters from './adapters/FetchAdapters.js';
+import MainInput from './components/MainInput.js';
 
 class App extends Component {
   constructor(props){
@@ -28,6 +29,7 @@ class App extends Component {
               Your browser is not supported!
             </video>
           </div>
+        <MainInput/>
       </div>
     );
   }
---
+++ src/components/MainInput.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default class MainInput extends React.Component{
+  constructor(){
+    super()
+    this.state={
+      value: ""
+    }
+  }
+
+  render(){
+    return(
+      <div className="MainInput">
+        <form className="MainInput__form">
+          <h2 className="MainInput__form__h2">To Get Started, Enter a Zipcode</h2>
+          <div className="MainInput__form__group">
+            <input type="text" id="zipinput" className="MainInput__form__group-input" placeholder="Enter Zip Code" required />
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
