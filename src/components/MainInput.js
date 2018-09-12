fb9311024d798cd09660b053a1e4548c7e2e3cf3
AustinPaley <austinpaley32@gmail.com>
2018-09-12 17:24:22 -0400

    Forgot Custom Validation Rule for Input - Now Added

--- src/components/MainInput.js
+++ src/components/MainInput.js
@@ -14,7 +14,7 @@ export default class MainInput extends React.Component{
         <form className="MainInput__form">
           <h2 className="MainInput__form__h2">To Get Started, Enter a Zipcode</h2>
           <div className="MainInput__form__group">
-            <input type="text" id="zipinput" className="MainInput__form__group-input" placeholder="Enter Zip Code" required />
+            <input type="text" id="zipinput" className="MainInput__form__group-input" placeholder="Enter Zip Code" pattern="[0-9]{5}" required />
           </div>
         </form>
       </div>
